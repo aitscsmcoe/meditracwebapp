4fb303daf104944a32930096d1642a0ae2178233
Abhimanu <aitscsmcoe@gmail.com>
2025-10-07 15:19:54 +0530

    feat: working MediTrac Electron + React setup with Firebase integration

---
+++ src/renderer/App.jsx
@@ -0,0 +1,4 @@
+﻿import React from "react";
+export default function App() {
+  return <div style={{padding:20}}>Welcome to MediTrac Desktop App</div>;
+}
---
+++ src/renderer/services/firebaseAdmin.js
@@ -0,0 +1,13 @@
+﻿import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyBoqRC58GOarQ1v3FSxlPT0wHlDseTxXno",
+  authDomain: "meditrack-admin-69386.firebaseapp.com",
+  projectId: "meditrack-admin-69386",
+  storageBucket: "meditrack-admin-69386.firebasestorage.app",
+  messagingSenderId: "189030134159",
+  appId: "1:189030134159:web:e3917f936ba2475e9b1bfa",
+  measurementId: "G-1Z7HGR774G"
+};
+const app = initializeApp(firebaseConfig);
+export const adminDb = getFirestore(app);
